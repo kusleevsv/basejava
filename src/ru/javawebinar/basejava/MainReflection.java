dc39415b928ea869516da3ea25d35a8672fdd41e
Sergey K <kusleevsv@alfastrah.ru>
2019-11-01 11:15:10 +0300

    added invoke r.toString via reflection

--- src/ru/javawebinar/basejava/MainReflection.java
+++ src/ru/javawebinar/basejava/MainReflection.java
@@ -3,17 +3,28 @@ package ru.javawebinar.basejava;
 import ru.javawebinar.basejava.model.Resume;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class MainReflection {
 
     public static void main(String[] args) throws IllegalAccessException {
-        Resume r = new Resume();
-        Field field = r.getClass().getDeclaredFields()[0];
+        Resume resume = new Resume();
+        Field field = resume.getClass().getDeclaredFields()[0];
         field.setAccessible(true);
         System.out.println(field.getName());
-        System.out.println(field.get(r));
-        field.set(r, "new_uuid");
+        System.out.println(field.get(resume));
+        field.set(resume, "new_uuid");
         // TODO : invoke r.toString via reflection
-        System.out.println(r);
+        for(Method method : resume.getClass().getMethods()) {
+            if(method.getName().equals("toString")) {
+                try {
+                    System.out.println("invoke r.toString via reflection " +  method.invoke(resume));
+                } catch (InvocationTargetException e) {
+                    System.out.println(e);
+                }
+            }
+        }
+        System.out.println(resume);
     }
 }
