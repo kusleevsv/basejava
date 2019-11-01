6ac49f944f7c32f438972bbb0d1a4ca64eb97b75
Куслеев Сергей Викторович <kusleevsv@alfastrah.ru>
2019-11-01 11:18:37 +0300

    added test methods in SortedArrayStorageTest.class

--- test/ru/javawebinar/basejava/storage/ArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/ArrayStorageTest.java
@@ -1,4 +1,5 @@
 package ru.javawebinar.basejava.storage;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,6 +11,7 @@ public class ArrayStorageTest {
     private final static Resume resume1 = new Resume("uuid1");
     private final static Resume resume2 = new Resume("uuid2");
     private final static Resume resume3 = new Resume("uuid3");
+    private final static String UUID4 = "uuid4";
 
     @Before
     public void setUp() {
@@ -31,21 +33,42 @@ public class ArrayStorageTest {
 
     @Test
     public void save() {
-        Resume resume4 = new Resume("uuid4");
-        storage.save(resume4);
+        Resume resume4 = add(UUID4);
         Assert.assertEquals(resume4, storage.get(resume4.getUuid()));
     }
 
     @Test
     public void upgrade() {
-        Resume resume4 = new Resume("uuid4");
-        storage.update(resume4);
-        Assert.assertEquals(resume4, storage.get(resume4.getUuid()));
+        Resume resume4 = add(UUID4);
+        Resume resume4new = new Resume(resume4.getUuid());
+        storage.update(resume4new);
+        Assert.assertEquals(resume4new, storage.get(resume4.getUuid()));
     }
 
     @Test(expected = NotExistStorageException.class)
     public void delete() {
-        storage.delete("uuid4");
-        storage.get("uuid4");
+        add(UUID4);
+        storage.delete(UUID4);
+        storage.get(UUID4);
+    }
+
+    private Resume add(String uuid) {
+        Resume resume4 = new Resume(uuid);
+        storage.save(resume4);
+        return resume4;
+    }
+
+    @Test
+    public void clear() {
+        storage.clear();
+        Assert.assertEquals(0, storage.size());
+    }
+
+    @Test
+    public void getAll() {
+        add(UUID4);
+        storage.delete(resume2.getUuid());
+        Resume[] resumes = {resume1, new Resume(UUID4), resume3};
+        Assert.assertArrayEquals(resumes, storage.getAll());
     }
 }
