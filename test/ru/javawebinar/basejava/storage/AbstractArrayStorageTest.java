314494fd30c0f9fd9c62c8d2a219d9146ab2f8e1
Sergey K <kusleevsv@alfastrah.ru>
2019-11-02 15:29:10 +0300

    added AbstractArrayStorageTest.class and test`s methods moved to it

---
+++ test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
@@ -0,0 +1,69 @@
+package ru.javawebinar.basejava.storage;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import ru.javawebinar.basejava.exception.NotExistStorageException;
+import ru.javawebinar.basejava.model.Resume;
+
+
+public abstract class AbstractArrayStorageTest {
+    Storage storage;
+    public final static Resume RESUME_1 = new Resume("uuid1");
+    public final static Resume RESUME_2 = new Resume("uuid2");
+    public final static Resume RESUME_3 = new Resume("uuid3");
+    public final static Resume RESUME_4 = new Resume("uuid4");
+
+    public AbstractArrayStorageTest(Storage storage) {
+        this.storage = storage;
+    }
+
+    @Before
+    public void setUp() {
+        storage.save(RESUME_1);
+        storage.save(RESUME_3);
+        storage.save(RESUME_2);
+    }
+
+    @Test
+    public void size_getSizeStorage_3() {
+        Assert.assertEquals(3, storage.size());
+    }
+
+    @Test
+    public void get_getResume_currentResume() {
+        Assert.assertEquals(RESUME_2, storage.get(RESUME_2.getUuid()));
+    }
+
+    @Test
+    public void save_addResume_getAddedResume() {
+        storage.save(RESUME_4);
+        Assert.assertEquals(RESUME_4, storage.get(RESUME_4.getUuid()));
+    }
+
+    @Test
+    public void upgrade_saveNewResume_getUpdatedResume() {
+        storage.save(RESUME_4);
+        Resume resume4new = new Resume(RESUME_4.getUuid());
+        storage.update(resume4new);
+        Assert.assertEquals(resume4new, storage.get(RESUME_4.getUuid()));
+    }
+
+    @Test(expected = NotExistStorageException.class)
+    public void delete_deleteResume_exception() {
+        storage.save(RESUME_4);
+        storage.delete(RESUME_4.getUuid());
+        storage.get(RESUME_4.getUuid());
+    }
+
+
+    @Test
+    public void clear_clearStorage_storageSizeZero() {
+        storage.clear();
+        Assert.assertEquals(0, storage.size());
+    }
+
+    @Test
+    public void getAll_saveAndDeleteResume_orderResume() {
+    }
+}
--- test/ru/javawebinar/basejava/storage/SortedArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/SortedArrayStorageTest.java
@@ -6,69 +6,17 @@ import org.junit.Test;
 import ru.javawebinar.basejava.exception.NotExistStorageException;
 import ru.javawebinar.basejava.model.Resume;
 
-public class SortedArrayStorageTest {
-    Storage storage;
-    private final static Resume resume1 = new Resume("uuid1");
-    private final static Resume resume2 = new Resume("uuid2");
-    private final static Resume resume3 = new Resume("uuid3");
-    private final static String UUID4 = "uuid4";
-
-    @Before
-    public void setUp() {
-        storage = new SortedArrayStorage();
-        storage.save(resume1);
-        storage.save(resume2);
-        storage.save(resume3);
+public class SortedArrayStorageTest extends AbstractArrayStorageTest {
+    public SortedArrayStorageTest() {
+        super(new SortedArrayStorage());
     }
 
+    @Override
     @Test
-    public void size() {
-        Assert.assertEquals(3, storage.size());
-    }
-
-    @Test
-    public void get() {
-        Assert.assertEquals(resume2, storage.get(resume2.getUuid()));
-    }
-
-    @Test
-    public void save() {
-        Resume resume4 = add(UUID4);
-        Assert.assertEquals(resume4, storage.get(resume4.getUuid()));
-    }
-
-    @Test
-    public void upgrade() {
-        Resume resume4 = add(UUID4);
-        Resume resume4new = new Resume(resume4.getUuid());
-        storage.update(resume4new);
-        Assert.assertEquals(resume4new, storage.get(resume4.getUuid()));
-    }
-
-    @Test(expected = NotExistStorageException.class)
-    public void delete() {
-        add(UUID4);
-        storage.delete(UUID4);
-        storage.get(UUID4);
-    }
-
-    private Resume add(String uuid) {
-        Resume resume4 = new Resume(uuid);
-        storage.save(resume4);
-        return resume4;
-    }
-
-    @Test
-    public void clear() {
-        storage.clear();
-        Assert.assertEquals(0, storage.size());
-    }
-
-    @Test
-    public void getAll() {
-        add(UUID4);
-        storage.delete(resume2.getUuid());
-        Resume[] resumes = {resume1, resume3, new Resume(UUID4)};
+    public void getAll_saveAndDeleteResume_orderResume() {
+        storage.save(RESUME_4);
+        storage.delete(RESUME_2.getUuid());
+        Resume[] resumes = {RESUME_1, RESUME_3, RESUME_4};
         Assert.assertArrayEquals(resumes, storage.getAll());
     }
 }
